a547242af7924fc1502f885bad1a33c576bb2288
0racularGh0st <nigel4tariang@gmail.com>
2020-11-30 23:09:11 +0530

    update config file

--- chatroom-client/src/config/index.js
+++ chatroom-client/src/config/index.js
@@ -1,5 +1,28 @@
 module.exports = {
-    SIGNALLING_SERVER : "http://localhost:3002",
-    PEER_SERVER: "nigeldavid-peer-serve.herokuapp.com",
-    ROOM_PATTERN: /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+    SIGNALLING_SERVER : "http://localhost:3002", //or your own signalling server
+    PEER_SERVER: "localhost:3001", // or your own peer server
+    ROOM_PATTERN: /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+    ICE_SERVERS: {
+        iceServers: [
+          {
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:stun1.l.google.com:19302",
+              "stun:stun2.l.google.com:19302",
+              "stun:stun3.l.google.com:19302"
+            ],
+          }
+          // You can get a turn server from https://xirsys.com/ for free with limited bandwidth though. 
+          // It's good enough for testing. Generate static turn credentials and fill them below
+          // ,
+          // {
+          //   username: "replace with your turn server's  username",
+          //   credential: "replace with your turn server's credential",
+          //   urls: [
+          //     "turn:<turn server uri>:<port>?transport=udp or tcp",
+          //   ],
+          // },
+        ],
+        sdpSemantics: "unified-plan",
+      }
 }
